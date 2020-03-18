890935d4095f5ad949540c9f1525a5fae7821d27
Jules Jehanno <julees.jehanno@courriel.ca>
2020-03-18 16:10:46 -0400

    Choose rotator to edit
    On peut maintenant choisir les objets que l'on veut modifier

---
+++ Assets/Editor/RotatorEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace com.technical.test
+{
+
+    public class RotatorEditor : ScriptableWizard
+    {
+        public static int size = 1;
+        public Rotator[] rotatorsToEdit = new Rotator[size];
+
+
+        [MenuItem("Window/Custom/Rotators Mass Setter")]
+        static void SelectAllOfRotatorWizard()
+        {
+            ScriptableWizard.DisplayWizard<RotatorEditor>("Rotators Mass Setter", "Validate changes");
+
+            // Select all rotators in the scene
+            /*
+            Rotator[] rotators = (Rotator[])GameObject.FindObjectsOfType<Rotator>();
+            List<GameObject> gameObjects = new List<GameObject>();
+            foreach (Rotator rotator in rotators)
+            {
+                gameObjects.Add(rotator.gameObject);
+            }
+            Selection.objects = gameObjects.ToArray();
+            */
+            
+        }
+
+        void OnWizardCreate()
+        {
+
+        }
+    }
+}
